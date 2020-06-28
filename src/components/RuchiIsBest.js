74cc3389218682e503353ef097821368ff33ce0c
Ruchi Priya <ruuchi13@gmail.com>
2020-06-28 13:49:58 +0530

    my first commit

---
+++ src/components/RuchiIsBest.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class RuchiIsBest extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hi, ruchi</h1>
+            </div>
+        )
+    }
+}
+
+export default RuchiIsBest
